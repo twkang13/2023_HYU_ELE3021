3c6448e73c3c822b24a04a31054d7167fedcf763
2021025205 <ktw020409@hanyang.ac.kr>
2023-05-19 05:09:29 +0000

    Project#2: pmanager

--- project02/pmanager.c
+++ project02/pmanager.c
@@ -6,67 +6,22 @@
 
 void panic(char*);
 int fork1(void);
+int getcmd(char*, int);
+void runcmd(char*, int, char*[]);
 
 int
 main(int argc, char *argv[])
 {
-    while(1){
-        printf(1, "pmanager> ");
+    static char buffer[MAXBUF] = {0, };
 
-        // Read buffer
-        char buffer[MAXBUF] = {0, };
-        memset(buffer, 0, MAXBUF);
-        gets(buffer, MAXBUF);
+    while(getcmd(buffer, MAXBUF) >= 0){
+        // exit
+        if(buffer[0] == 'e' && buffer[1] == 'x' && buffer[2] == 'i' && buffer[3] == 't')
+            break;
 
-        // Pares arguments - arg[0] : command, arg[1] : argument 1, arg[2] : argument 2
-        char arg[3][51] = {0, };
-        int argNum = 0, len = (int)strlen(buffer);
-        // TODO : buffer[len]의 값 확인 
-        for(int i = 0, j = 0; i <= len; i++, j++){
-            if(buffer[i] == '\n' || buffer[i] == 32){
-                arg[argNum][j] = '\0';
-                ++argNum;
-                j = -1;
-            }
-            else
-                arg[argNum][j] = buffer[i];
-        }
-        
-        if(!strcmp(arg[0], "list") && argNum == 1){
-            if(plist() < 0)
-                printf(1, "ERROR : list failed.\n");
-        }
-        else if(!strcmp(arg[0], "kill") && argNum == 2){
-            // pid = arg[1]
-            if(kill(atoi(arg[1])) < 0)
-                printf(1, "ERROR : kill failed.\n");
-            else
-                printf(1, "kill success.\n");
-        }
-        else if(!strcmp(arg[0], "execute") && argNum == 3){
-            // path = arg[1]    stacksize = arg[2]
-            if(fork1() == 0){
-                exec2(arg[1], argv, atoi(arg[2]));
-                exit();
-            }
-            // TODO : background 실행 구현
-            continue;
-        }
-        else if(!strcmp(arg[0], "memlim") && argNum == 3){
-            // pid = arg[1]     limit = arg[2]
-            if(setmemorylimit(atoi(arg[1]), atoi(arg[2])) < 0)
-                printf(1, "ERROR : memlim failed.\n");
-            else
-                printf(1, "memlim success.\n");
-        }
-        
-        else if(!strcmp(arg[0], "exit") && argNum == 1){
-            exit();
-        }
-        // Error handling   ex) Nothing typed, Invalid argument
-        else{
-            printf(1, "Invalid command! Try again.\n");
-        }
+        if(fork1() == 0)
+            runcmd(buffer, argc, argv);
+        wait();
     }
 
     exit();
@@ -88,4 +43,68 @@ fork1(void)
   if(pid == -1)
     panic("fork");
   return pid;
+}
+
+// Read buffer
+int
+getcmd(char *buf, int nbuf)
+{
+  printf(2, "pmanager> ");
+  memset(buf, 0, nbuf);
+  gets(buf, nbuf);
+  if(buf[0] == 0) // EOF
+    return -1;
+  return 0;
+}
+
+// Run pmanager
+void
+runcmd(char *buffer, int argc, char *argv[])
+{
+    // Pares arguments - arg[0] : command, arg[1] : argument 1, arg[2] : argument 2
+    char arg[3][51] = {0, };
+    int argNum = 0, len = (int)strlen(buffer);
+    // TODO : buffer[len]의 값 확인 
+    for(int i = 0, j = 0; i <= len; i++, j++){
+        if(buffer[i] == '\n' || buffer[i] == 32){
+            arg[argNum][j] = '\0';
+            ++argNum;
+            j = -1;
+        }
+        else
+            arg[argNum][j] = buffer[i];
+    }
+            
+    if(!strcmp(arg[0], "list") && argNum == 1){
+        if(plist() < 0)
+            printf(1, "ERROR : list failed.\n");
+    }
+    else if(!strcmp(arg[0], "kill") && argNum == 2){
+        // pid = arg[1]
+        if(kill(atoi(arg[1])) < 0)
+            printf(1, "ERROR : kill failed.\n");
+        else
+            printf(1, "kill success.\n");
+    }
+    else if(!strcmp(arg[0], "execute") && argNum == 3){
+        // path = arg[1]    stacksize = arg[2]
+        if(fork1() == 0){
+            exec2(arg[1], argv, atoi(arg[2]));
+            exit();
+        }
+        // TODO : background 실행 구현
+    }
+    else if(!strcmp(arg[0], "memlim") && argNum == 3){
+        // pid = arg[1]     limit = arg[2]
+        if(setmemorylimit(atoi(arg[1]), atoi(arg[2])) < 0)
+            printf(1, "ERROR : memlim failed.\n");
+        else
+            printf(1, "memlim success.\n");
+    }
+    // Error handling   ex) Nothing typed, Invalid argument
+    else{
+        printf(1, "Invalid command! Try again.\n");
+    }
+
+    exit();
 }
