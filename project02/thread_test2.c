b66387ea225c86b79c4004511524ff854157666f
2021025205 <ktw020409@hanyang.ac.kr>
2023-05-28 06:53:27 +0000

    Project#2: add new test feature

--- project02/thread_test2.c
+++ project02/thread_test2.c
@@ -6,12 +6,30 @@
 #define NTHREAD 5
 
 thread_t thread[NTHREAD];
+thread_t thread2[NTHREAD];
 int shared = 0;
+
+void *thread_func2(void *arg)
+{
+    shared += (int)arg;
+    printf(1, "thread : %d(%d)\n", shared, (int)arg*10);
+    thread_exit(arg);
+    return 0;
+}
 
 void *thread_func(void *arg)
 {
     shared += (int)arg;
     printf(1, "thread : %d(%d)\n", shared, (int)arg);
+    if ((int)arg == 1){
+        for(int i = 0; i < NTHREAD; i++)
+            thread_create(&thread2[i], thread_func2, (void *)((int)arg*10));
+        for(int i = 0; i < NTHREAD; i++){
+            int retval;
+            thread_join(thread2[i], (void**)&retval);
+            printf(1, "thread joined : %d\n", retval);
+        }
+    }
     thread_exit(arg);
     return 0;
 }
